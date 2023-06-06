0560563dfb9711111e1e8796e3fe802162393e1f
SlavicSandwich <78306203+SlavicSandwich@users.noreply.github.com>
2023-06-06 13:15:48 +0300

    finish the test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  const oldest = arr.sort((a, b) => {
+    if (!("yearOfDeath" in a)) {
+      a.yearOfDeath = new Date().getFullYear();
+    }
+    if (!("yearOfDeath" in b)) {
+      b.yearOfDeath = new Date().getFullYear();
+    }
+    const lastPerson = a.yearOfDeath - a.yearOfBirth;
+    const nextPerson = b.yearOfDeath - b.yearOfBirth;
+    return lastPerson > nextPerson ? -1 : 1;
+  });
+  return oldest[0];
 };
 
 // Do not edit below this line
